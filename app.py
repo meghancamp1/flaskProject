76419f1d4a781c75d856269f43dcffa64daed546
meghancamp1 <meghan.camp@my.jcu.edu.au>
2021-10-31 21:54:41 +1000

    Add celsius_to_fahrenheit function

--- app.py
+++ app.py
@@ -14,5 +14,12 @@ def greet(name=""):
     return "Hello {}".format(name)
 
 
+@app.route('/f/<celsius>')
+def celsius_to_fahrenheit(celsius=0):
+    celsius_temperature = float(celsius)
+    fahrenheit = celsius_temperature * 9 / 5 + 32
+    return f"{celsius} C is {fahrenheit} F"
+
+
 if __name__ == '__main__':
     app.run()
